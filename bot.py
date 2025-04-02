d04ec1edef44a103079b895a6c7b6de18dd400af
Bjarnos <braverpsx@gmail.com>
2025-04-02 10:54:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# Imports
 import requests
 import dotenv, os
 import re
@@ -5,14 +6,18 @@ import time
 import threading
 from bs4 import BeautifulSoup
 
+# Preloads
 dotenv.load_dotenv()
-
 session = requests.Session()
 
-login_url = "https://chat.jonazwetsloot.nl/login"
-actionlogin_url = "https://chat.jonazwetsloot.nl/actionlogin"
-timeline_url = "https://chat.jonazwetsloot.nl/timeline"
-send_message_url = "https://chat.jonazwetsloot.nl/api/v1/message"
+# Constants
+url = "https://chat.jonazwetsloot.nl"
+api_url = f"{url}/api/v1"
+login_url = f"{url}/login"
+actionlogin_url = f"{url}/actionlogin"
+timeline_url = f"{url}/timeline"
+send_message_url = f"{api_url}/message"
+like_url = f"{api_url}/like"
 
 headers = { 
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,application/json,text/plain,*/*;q=0.8",
@@ -26,14 +31,49 @@ headers = {
     "Content-Type": "application/x-www-form-urlencoded",
 }
 
+# Variables
 message_cache = {}
+
+# API
+def create_post(message):
+    """ Sends a message """
+    key = get_key()
+    if not key:
+        print("Failed to retrieve key.")
+        return
+
+    data = {
+        "message": message,
+        "attachments": "",
+        "name": user,
+        "key": key
+    }
+    response = session.post(send_message_url, data=data, headers=headers)
+    print(f"Response Status Code (Send Message): {response.status_code}")
+
+def like(message_id, value):
+    """ Likes a message """
+    key = get_key()
+    if not key:
+        print("Failed to retrieve key.")
+        return
+
+    data = {
+        "id": message_id,
+        "like": True if value is None else value,
+        "name": user,
+        "key": key
+    }
+    response = session.post(like_url, data=data, headers=headers)
+    print(f"Response Status Code (Like Message): {response.status_code}")
 
 # Custom Message class
 class Message:
-    def __init__(self, time, text, sender):
+    def __init__(self, time, text, sender, id):
         self.time = time
         self.text = text
         self.sender = sender
+        self.id = id
 
     def __repr__(self):
         if len(self.text) > 30:
@@ -41,6 +81,9 @@ class Message:
         else:
             return f"Message(from {self.sender}): {self.text}"
 
+    def like(self, value):
+        like(self.id, value)
+    
     # Return time as formatted string?
     def get_time(self):
         return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(self.time))
@@ -71,12 +114,16 @@ def extract_messages(html):
         time_element = message_div.find('p', class_='time')
         content_element = message_div.find('div', class_='content')
         user_element = message_div.find('a', class_='username')
+        
+        message_id_element = message_div.find('button', class_='submit inverted message-menu-share-button')
+        message_id = message_id_element['data-id'] if message_id_element and 'data-id' in message_id_element.attrs else None
+        print(message_id)
         if time_element and content_element:
             time_text = time_element.text.strip()
             content_text = content_element.text.strip()
             user_text = user_element.text.strip()
             time_in_seconds = time.time() - convert_to_seconds(time_text)
-            message = Message(time_in_seconds, content_text, user_text)
+            message = Message(time_in_seconds, content_text, user_text, message_id)
             messages.append(message)
     return messages
 
@@ -106,7 +153,6 @@ class FunctionBinder:
                 response = requests.get(timeline_url, headers=headers)
                 if response.status_code == 200:
                     messages = extract_messages(response.text)
-                    print(messages)
                     if messages:
                         if self.last_checked_time is None:
                             self.last_checked_time = messages[0].time
@@ -120,7 +166,6 @@ class FunctionBinder:
                                         self._run_bound_functions(message)
 
                             self.last_checked_time = messages[0].time
-                            print(message_cache)
             except Exception as e:
                 print(f"Error checking for new posts: {e}")
             time.sleep(10)
@@ -158,29 +203,12 @@ def login(username, password):
     return response.status_code == 200
 
 def get_key():
-    """ Extracts the key needed for sending messages """
+    """ Extracts the key needed for Authorization """
     response = session.get(timeline_url, headers=headers)
     match = re.search(r'<input[^>]+name="key"[^>]+value="([^"]+)"', response.text)
     if match:
         return match.group(1)
     return None
-
-# API
-def send_message(message):
-    """ Sends a message using the chat API """
-    key = get_key()
-    if not key:
-        print("Failed to retrieve key.")
-        return
-
-    data = {
-        "message": message,
-        "attachments": "",
-        "name": user,
-        "key": key
-    }
-    response = session.post(send_message_url, data=data, headers=headers)
-    print(f"Response Status Code (Send Message): {response.status_code}")
 
 # Example Core
 get_php_session()
@@ -188,12 +216,11 @@ if login(os.environ.get('user'), os.environ.get('pass')):
     # Example client
     binder = FunctionBinder()
 
-    def ping(message):
-        #print("Found a message!")
-        print("Content text: " + message.text)
-        #print("Sender: " + message.sender)
-    binder.bind(ping)
+    def dolike(message):
+        print("Found a message!")
+        message.like()
+    binder.bind(dolike)
 
     binder.start_checking()
     
-    send_message("||This message was automatically generated by a selfbot.||")
+    create_post("||This message was automatically generated by a selfbot.||")
