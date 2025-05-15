ed2e0d99c6c05dad0be41e12fb7e6ee115330330
Bjarnos <braverpsx@gmail.com>
2025-05-15 21:28:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ password = os.environ.get('pass')
 if bot.login(username, password):
     connections = bot.ConnectionService
     def f1(message):
+        print("message?")
+        print(message.sender)
         if message.sender == "Bjarnos":
             message.reply("Hi Bjarnos!\n-# This message was created with a selfbot.")
     connections.bind_to_public_post(f1)
