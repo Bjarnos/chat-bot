d14bba9b0242bd5c6e9d52b665dbdadee6caeec7
Bjarnos <braverpsx@gmail.com>
2025-05-16 09:44:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,63 +1,70 @@
-import dotenv, os
-from ChatSelfbot import BotService
-DMMessage = Classes.DMMessage
-bot = BotService.create_bot()
+# This script was written with a Docs AI, coming soon!
+import dotenv
+import os
+import random
+from ChatSelfbot import BotService, Classes
 
 dotenv.load_dotenv()
+
+bot = BotService.create_bot()
 username = os.environ.get('user')
 password = os.environ.get('pass')
+
 if bot.login(username, password):
     connections = bot.ConnectionService
     messages = bot.MessageService
 
-    players = {}
+    # Local variables for storing player data
+    player_data = {}
 
-    def start_game(username):
-        players[username] = {
-            'health': 100,
-            'cash': 0,
-            'weapon': 'Wooden Sword',
-            'damage': 10
-        }
-        return f"Welcome to the RPG, {username}! You have started with a {players[username]['weapon']}."
-
-    def battle(username):
-        if username not in players:
-            return "You need to start the game first! Type 'start' to begin."
-        
-        monster_health = 50
-        player_damage = players[username]['damage']
-        
-        while monster_health > 0 and players[username]['health'] > 0:
-            attack = random.randint(5, player_damage)
-            monster_health -= attack
-            response = f"You dealt {attack} damage to the monster! Monster health: {monster_health}"
-            
-            if monster_health <= 0:
-                players[username]['cash'] += 20
-                return response + f"\nYou defeated the monster! You earned 20 cash. Total cash: {players[username]['cash']}"
-            
-            monster_attack = random.randint(5, 15)
-            players[username]['health'] -= monster_attack
-            response += f"\nThe monster attacked you for {monster_attack} damage! Your health: {players[username]['health']}"
-            
-            if players[username]['health'] <= 0:
-                return response + "\nYou have been defeated! Game over."
-        
-        return response
-
-    def f1(message: DMMessage):
-        username = message.sender.username
-        content = message.content.lower()
-
-        if content == 'start':
-            response = start_game(username)
-        elif content == 'battle':
-            response = battle(username)
+    def start_game(message: Classes.DMMessage):
+        player_id = message.sender_id
+        if player_id not in player_data:
+            player_data[player_id] = {'cash': 0, 'sword_damage': 1}
+            messages.reply(message.id, "Welcome to the RPG! You can battle monsters to earn cash.")
         else:
-            response = "Type 'start' to begin your adventure or 'battle' to fight a monster."
+            messages.reply(message.id, "You are already in the game! Type 'battle' to fight a monster.")
 
-        message.reply(response)
+    def battle(message: Classes.DMMessage):
+        player_id = message.sender_id
+        if player_id not in player_data:
+            messages.reply(message.id, "You need to start the game first! Type 'start' to begin.")
+            return
+        
+        monster_health = random.randint(5, 15)
+        player_damage = player_data[player_id]['sword_damage']
+        
+        while monster_health > 0:
+            monster_health -= player_damage
+            if monster_health <= 0:
+                player_data[player_id]['cash'] += 10  # Reward for defeating the monster
+                messages.reply(message.id, f"You defeated the monster! You earned 10 cash. Total cash: {player_data[player_id]['cash']}")
+                return
+            # Simulate monster attack (optional)
+            messages.reply(message.id, f"The monster has {monster_health} health left. You deal {player_damage} damage!")
 
-    connections.bind_to_any_dm(f1)
-    connections.start_checking_dms()  # required!
+    def buy_sword(message: Classes.DMMessage):
+        player_id = message.sender_id
+        if player_id not in player_data:
+            messages.reply(message.id, "You need to start the game first! Type 'start' to begin.")
+            return
+        
+        if player_data[player_id]['cash'] >= 50:  # Cost of the sword
+            player_data[player_id]['sword_damage'] += 1
+            player_data[player_id]['cash'] -= 50
+            messages.reply(message.id, f"You bought a sword! Your damage is now {player_data[player_id]['sword_damage']}.")
+        else:
+            messages.reply(message.id, "You don't have enough cash to buy a sword.")
+
+    def handle_message(message: Classes.DMMessage):
+        if message.content.lower() == 'start':
+            start_game(message)
+        elif message.content.lower() == 'battle':
+            battle(message)
+        elif message.content.lower() == 'buy sword':
+            buy_sword(message)
+        else:
+            messages.reply(message.id, "Type 'start' to begin the game, 'battle' to fight, or 'buy sword' to purchase a sword.")
+
+    connections.bind_to_any_dm(handle_message)
+    connections.start_checking_dms()  # Required to check for new DMs
